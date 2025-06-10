5370e1a6c0c34113539452b526e596fa22695f2f
Carson Henrich <carson03henrich@gmail.com>
2025-06-09 17:25:20 -0700

    Fix src/checks/bee.nix for #39

--- src/checks/bee.nix
+++ src/checks/bee.nix
@@ -1,33 +1,35 @@
-{
-  nixpkgs,
-  root,
-}: let
+{ nixpkgs
+, root
+,
+}:
+let
   l = nixpkgs.lib // builtins;
 
   inherit (root) beeModule;
 
-  check = locatedConfig: let
-    checked = l.nixos.evalModules {
-      modules = [
-        locatedConfig
-        beeModule
-        {
-          config._module.check = true;
-          config._module.freeformType = l.types.unspecified;
-        }
-      ];
-    };
+  check = locatedConfig:
+    let
+      checked = l.nixos.evalModules {
+        modules = [
+          locatedConfig
+          beeModule
+          {
+            config._module.check = false;
+            config._module.freeformType = l.mkForce null;
+          }
+        ];
+      };
 
-    failedAsserts = map (x: x.message) (l.filter (x: !x.assertion) checked.config.bee._alerts);
+      failedAsserts = map (x: x.message) (l.filter (x: !x.assertion) checked.config.bee._alerts);
 
-    asserted =
-      if failedAsserts != []
-      then throw "\nHive's layer sanitation boundary: \n${l.concatStringsSep "\n" (map (x: "- ${x}") failedAsserts)}"
-      else checked;
-  in
+      asserted =
+        if failedAsserts != [ ]
+        then throw "\nHive's layer sanitation boundary: \n${l.concatStringsSep "\n" (map (x: "- ${x}") failedAsserts)}"
+        else checked;
+    in
     assert l.isAttrs asserted; {
       inherit locatedConfig;
       evaled = asserted;
     };
 in
-  check
+check
